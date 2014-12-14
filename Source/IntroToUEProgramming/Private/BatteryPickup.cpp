abf5f66f2b05b18cb8a144adf4f0ae57ea5ee8d8
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-12-14 17:17:23 +0200

    battery pickup definition

--- Source/IntroToUEProgramming/Private/BatteryPickup.cpp
+++ Source/IntroToUEProgramming/Private/BatteryPickup.cpp
@@ -3,6 +3,19 @@
 #include "IntroToUEProgramming.h"
 #include "BatteryPickup.h"
 
+ABatteryPickup::ABatteryPickup(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+    // default powerlevel of battery
+    PowerLevel = 150;
+    
+    
+}
 
-
-
+void ABatteryPickup::OnPickedUp_Implementation()
+{
+    Super::OnPickedUp_Implementation();
+    
+    // destroy battery
+    Destroy();
+}
--- Source/IntroToUEProgramming/Public/BatteryPickup.h
+++ Source/IntroToUEProgramming/Public/BatteryPickup.h
@@ -14,7 +14,13 @@ class INTROTOUEPROGRAMMING_API ABatteryPickup : public APickup
 	GENERATED_BODY()
 	
 public:
+    ABatteryPickup(const FObjectInitializer& ObjectInitializer);
+    
+    // Amount of power battery gives to the player
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Power)
+    float PowerLevel;
 	
-	
-	
+    // Override the OnPickedUp(use _Implementation because
+    // this is a BlueprintNativeEvent)
+    void OnPickedUp_Implementation() OVERRIDE;
 };
